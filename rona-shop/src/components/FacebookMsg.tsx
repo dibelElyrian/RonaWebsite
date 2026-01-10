dcffe7763fb993cad5b4b46ee168942c3ae6924f
Ryan <ryan.serdan@infosoft.com.ph>
2026-01-10 13:48:43 +0800

    Add support for Facebook SDK initialization
    - 🛡️ Improved compatibility with Facebook Messenger by ensuring the system recognizes Facebook's setup process.
    - 🧩 Reduced risk of errors when connecting to Facebook chat features.

--- rona-shop/src/components/FacebookMsg.tsx
+++ rona-shop/src/components/FacebookMsg.tsx
@@ -2,6 +2,12 @@
 
 import { useEffect } from 'react'
 
+declare global {
+  interface Window {
+    fbAsyncInit: () => void;
+  }
+}
+
 export default function FacebookMsg() {
   useEffect(() => {
     // Inject FB SDK
